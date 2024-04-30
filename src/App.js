8a916b8556fa3ce62e93c241b4bc15b99148ea8d
Ritesh <sagarsaha@outlook.in>
2024-05-01 04:18:02 +0530

    initialize upload

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <header> Teri maa ki chut</header>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Red
         </a>
       </header>
     </div>
